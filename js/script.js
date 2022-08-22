2ae7e7eab7182ec43d67ef5a78d015891e520507
Your Nameyuliiazav <didiky1993@gmail.com>
2022-08-22 17:02:23 +0300

    edited JS to leave 1 option

--- js/script.js
+++ js/script.js
@@ -1,61 +1,41 @@
-let firstNumber = +prompt('Enter your first number');
-let secondNumber = +prompt('Enter your second number');
-let operation = prompt('Choose an operation (*/-+)');
-let result = '';
-
-   function calcResult(firstNumber, secondNumber, operation) {
-     return eval(`${firstNumber}` + operation + `${secondNumber}`);
- }
+  let numFirst = +prompt('Enter your first number');
+  let numSecond = +prompt('Enter your second number');
+  let operator = prompt('Choose an operation (+, -, *, /, %)');
+  let result = '';
 
 
- if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
-   alert('This operation does not exist. Try again!');
- } else {
-     result = calcResult(firstNumber, secondNumber, operation);
+  function calcResult(numFirst, numSecond, operator) {
+      switch (operator) {
+          case '+':
+              return numFirst + numSecond;
+          case '-':
+              return numFirst - numSecond;
+          case '*':
+              return numFirst * numSecond;
+          case '/':
+              return numFirst / numSecond;
+          case '<':
+              return numFirst < numSecond;
+          case '>':
+              return numFirst > numSecond;
+      }
+  }
 
-    if (isNaN(result)) {
-         alert('enter valid numbers only!');
+   if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
+        alert('This operation does not exist. Try again!');
     } else {
+        result = calcResult(numFirst, numSecond, operator);
 
-        alert(`Your result is ${result}`);}
+        if (isNaN(numFirst)  &&  (numSecond)) {  // isNaN(result)
+            alert('enter valid numbers only!');
 
- }
-////////////////////////
-//
-//   let numFirst = +prompt('Введите первое число');
-//   let numSecond = +prompt('Введите второе число');
-//   let operator = prompt('Введите знак операции (+, -, *, /, %)'); // любой: +, -, *, /, %
-//   let result = '';
-//
-//
-//   function calcResult(numFirst, numSecond, operator) {
-//       switch (operator) {
-//           case '+':
-//               return numFirst + numSecond;
-//           case '-':
-//               return numFirst - numSecond;
-//           case '*':
-//               return numFirst * numSecond;
-//           case '/':
-//               return numFirst / numSecond;
-//           case '<':
-//               return numFirst < numSecond;
-//           case '>':
-//               return numFirst > numSecond;
-//       }
-//   }
-//
-//   if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
-//       alert('This operation does not exist. Try again!');
-//   } else {
-//       result = calcResult(numFirst, numSecond, operator);
-//
-//       if (isNaN(numFirst)  &&  (numSecond)) {  // isNaN(result)
-//           alert('enter valid numbers only!');
-//
-//       } else {
-//
-//           alert(`Your result is ${result}`);}
-//
-//   }
-//
+        } else {
+
+            alert(`Your result is ${result}`);}
+
+    }
+
+
+
+
+
