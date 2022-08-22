f8ef3993635dcd949faeb9cf4729f6abfeb96e82
Your Nameyuliiazav <didiky1993@gmail.com>
2022-08-22 11:34:38 +0300

    added option 2 of the task and commented

--- js/script.js
+++ js/script.js
@@ -3,21 +3,59 @@ let secondNumber = +prompt('Enter your second number');
 let operation = prompt('Choose an operation (*/-+)');
 let result = '';
 
-function calcResult(firstNumber, secondNumber, operation) {
-    return eval(`${firstNumber}` + operation + `${secondNumber}`);}
+   function calcResult(firstNumber, secondNumber, operation) {
+     return eval(`${firstNumber}` + operation + `${secondNumber}`);
+ }
 
 
-if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
-    alert('This operation does not exist. Try again!');
-} else {
-    result = calcResult(firstNumber, secondNumber, operation);
+ if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
+   alert('This operation does not exist. Try again!');
+ } else {
+     result = calcResult(firstNumber, secondNumber, operation);
 
     if (isNaN(result)) {
-        alert('enter valid numbers only!');
+         alert('enter valid numbers only!');
     } else {
 
-    alert(`Your result is ${result}`);}
+        alert(`Your result is ${result}`);}
 
-}
-
-
+ }
+////////////////////////
+//
+//   let numFirst = +prompt('Введите первое число');
+//   let numSecond = +prompt('Введите второе число');
+//   let operator = prompt('Введите знак операции (+, -, *, /, %)'); // любой: +, -, *, /, %
+//   let result = '';
+//
+//
+//   function calcResult(numFirst, numSecond, operator) {
+//       switch (operator) {
+//           case '+':
+//               return numFirst + numSecond;
+//           case '-':
+//               return numFirst - numSecond;
+//           case '*':
+//               return numFirst * numSecond;
+//           case '/':
+//               return numFirst / numSecond;
+//           case '<':
+//               return numFirst < numSecond;
+//           case '>':
+//               return numFirst > numSecond;
+//       }
+//   }
+//
+//   if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
+//       alert('This operation does not exist. Try again!');
+//   } else {
+//       result = calcResult(numFirst, numSecond, operator);
+//
+//       if (isNaN(numFirst)  &&  (numSecond)) {  // isNaN(result)
+//           alert('enter valid numbers only!');
+//
+//       } else {
+//
+//           alert(`Your result is ${result}`);}
+//
+//   }
+//
