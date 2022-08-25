469143a0b881bdb8ce35e710004d929a7d1e798c
Your Nameyuliiazav <didiky1993@gmail.com>
2022-08-25 12:48:07 +0300

    edited HW, fixed mistakes

--- js/script.js
+++ js/script.js
@@ -1,41 +1,29 @@
-  let numFirst = +prompt('Enter your first number');
-  let numSecond = +prompt('Enter your second number');
-  let operator = prompt('Choose an operation (+, -, *, /, %)');
-  let result = '';
+let numFirst = +prompt('Enter your first number');
+let numSecond = +prompt('Enter your second number');
+let operator = prompt('Choose an operation (+, -, *, /, %)');
+let result = '';
 
 
-  function calcResult(numFirst, numSecond, operator) {
-      switch (operator) {
-          case '+':
-              return numFirst + numSecond;
-          case '-':
-              return numFirst - numSecond;
-          case '*':
-              return numFirst * numSecond;
-          case '/':
-              return numFirst / numSecond;
-          case '<':
-              return numFirst < numSecond;
-          case '>':
-              return numFirst > numSecond;
-      }
-  }
+if (isNaN(numFirst) || isNaN(numSecond)) {
+    alert('enter valid numbers only!');
+} else if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
+    alert('This operation does not exist. Try again!');
+} else {
+    result = calcResult(numFirst, numSecond, operator);
+    alert(`Your result is ${result}`);
+}
 
-   if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
-        alert('This operation does not exist. Try again!');
-    } else {
-        result = calcResult(numFirst, numSecond, operator);
 
-        if (isNaN(numFirst)  &&  (numSecond)) {  // isNaN(result)
-            alert('enter valid numbers only!');
-
-        } else {
-
-            alert(`Your result is ${result}`);}
+function calcResult(numFirst, numSecond, operator) {
+    switch (operator) {
+        case '+':
+            return numFirst + numSecond;
+        case '-':
+            return numFirst - numSecond;
+        case '*':
+            return numFirst * numSecond;
+        case '/':
+            return numFirst / numSecond;
 
     }
-
-
-
-
-
+}
